e6cc8b8cd51bcc4631323939b4e45aafaf667d84
Jordan Eremieff <jeremieff@gmail.com>
2021-06-05 21:05:25 +1000

    Initial commit. Create ST plugin for DjHTML with configurable indentation and auto-indent on save.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# sublime-djhtml
-Sublime Text plugin for DjHTML
+# Sublime DjHTML
+
+A plugin for Sublime Text to integrate [DjHTML](https://github.com/rtts/djhtml).
+
+- *tabwidth* (default=`4`):
+    The number of spaces used in the indentation.
+
+- `indent_on_save` (default=`false`)
+    Run indentation automatically on file save.
+
+- `enabled_syntax` (default=`[Packages/HTML/HTML.sublime-syntax, Packages/Djaneiro/Syntaxes/HTML (Django).tmLanguage`)
+    A list of file [syntax definitions](https://www.sublimetext.com/docs/syntax.html) eligible for auto-identation when `indent_on_save` is enabled.
---
+++ main.py
@@ -0,0 +1,24 @@
+import sublime
+import sublime_plugin
+from sublime import load_settings
+
+from .vendor.djhtml.modes import DjHTML
+
+
+SETTINGS = load_settings("sublime-djhtml.sublime-settings")
+
+
+class DjhtmlIndentCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        tabwidth = SETTINGS.get("tabwidth", 4)
+        region = sublime.Region(0, self.view.size())
+        file_data = self.view.substr(region)
+        self.view.replace(edit, region, DjHTML(file_data).indent(tabwidth))
+
+
+class DjhtmlIndentOnSaveListener(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        if SETTINGS.get("indent_on_save") and view.settings().get(
+            "syntax"
+        ) in SETTINGS.get("enabled_syntax", []):
+            view.run_command("djhtml_indent")
