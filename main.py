b83580e7fb97ebe3e52ac8934d1061c0e85156e2
Jordan Eremieff <jeremieff@gmail.com>
2021-06-05 23:50:22 +1000

    Fix settings loading

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import os
 import sys
 import sublime
 import sublime_plugin
-from sublime import load_settings
+
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from vendor.djhtml.modes import DjHTML
 
-SETTINGS = load_settings("sublime_djhtml.sublime-settings")
+
+SUBLIME_SETTINGS = "sublime_djhtml.sublime-settings"
 
 
 class DjhtmlIndentCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        tabwidth = SETTINGS.get("tabwidth", 4)
+        settings = sublime.load_settings(SUBLIME_SETTINGS)
+        tabwidth = settings.get("tabwidth", 4)
         region = sublime.Region(0, self.view.size())
         file_data = self.view.substr(region)
         indented = DjHTML(file_data).indent(tabwidth)
@@ -22,7 +24,8 @@ class DjhtmlIndentCommand(sublime_plugin.TextCommand):
 
 class DjhtmlIndentOnSaveListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
-        if SETTINGS.get("indent_on_save") and view.settings().get(
+        settings = sublime.load_settings(SUBLIME_SETTINGS)
+        if settings.get("indent_on_save") and view.settings().get(
             "syntax"
-        ) in SETTINGS.get("enabled_syntax", []):
+        ) in settings.get("enabled_syntax", []):
             view.run_command("djhtml_indent")
